22cc13b4358be73d9fc103f7d4eb385d04492e04
RoshRoslin <RoshRoslin@gmail.com>
2021-05-07 15:34:56 -0700

    source code to master

---
+++ src/data.js
@@ -0,0 +1,39 @@
+const reviews = [
+  {
+    id: 1,
+    name: 'susan smith',
+    job: 'web developer',
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, sint?',
+  },
+  {
+    id: 2,
+    name: 'anna johnson',
+    job: 'web designer',
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg',
+    text:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore, laudantium repellat consequatur explicabo harum.',
+  },
+  {
+    id: 3,
+    name: 'peter jones',
+    job: 'intern',
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+    text:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi expedita nemo vel quod consequuntur consectetur nostrum odio, et dolorum!',
+  },
+  {
+    id: 4,
+    name: 'bill anderson',
+    job: 'the boss',
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+    text:
+      ' Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex maxime voluptatum dignissimos doloribus! Debitis, quas culpa deserunt ipsam dolores obcaecati ullam nemo saepe earum animi libero, id, facere aperiam laudantium?',
+  },
+];
+
+export default reviews;
